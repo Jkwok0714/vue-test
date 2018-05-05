204f1d8192c74d7ae10b9b5181070033e62e9388
Justin <jkwok0714@gmail.com>
2018-05-04 22:56:14 -0700

    Dynamic render components

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <ul>
-        <li> Todo A </li>
-        <li> Todo B </li>
-        <li> Todo C </li>
-    </ul>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <TodoItem v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.title"></TodoItem>
   </div>
 </template>
 
 <script type = "text/javascript" >
 
+import TodoItem from './TodoItem';
+
 export default {
+  props: ['todos'],
+  components: {
+    TodoItem,
+  },
 };
 </script>
+
 <style>
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="todo-card">
+    <div>
+      <div>
+        {{ todo.title }}
+      </div>
+      <div>
+        {{ todo.project }}
+      </div>
+      <div>
+        <span>
+          <i class='edit icon'></i>
+        </span>
+      </div>
+    </div>
+    <div v-show="todo.done">
+      Completed
+    </div>
+    <div v-show="!todo.done">
+      Complete
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  props: ['todo'],
+};
+</script>
